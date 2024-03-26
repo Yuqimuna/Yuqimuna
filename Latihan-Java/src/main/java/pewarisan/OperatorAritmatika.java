7a786e22cd09649b6b9e6840b36e1fd3e3516b16
Yuqimuna <164319043+Yuqimuna@users.noreply.github.com>
2024-03-26 07:57:38 +0700

    Add files via upload

---
+++ Latihan-Java/src/main/java/pewarisan/OperatorAritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pewarisan;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class OperatorAritmatika {
+    
+    public static void main (String [] args){
+        // beklarasi dan inisialisasi variable
+        int i = 9 ;
+        int j = 4 ;
+        // menampilkan nilai variable
+        System.out.println ("Nilai Variable :" );
+        System.out.println (" i = "+ i );
+        System.out.println (" j = "+ j );
+        // Menggunakan Operator Penjumlahan
+        System.out.println (" Penjumlahan : " );
+        System.out.println (" i + j = "+ (i + j));
+        // Menggunakan operator Ppengurangan
+        System.out.println (" Pengurangan :");
+        System.out.println (" i - j = "+ (i-j));
+        // Menggunakan operator Perkalian
+        System.out.println (" Perkalian :");
+        System.out.println (" i * j = "+ (i*j));
+        // Menggunakan Operator pembagian
+        System.out.println (" Pembagian :");
+        System.out.println (" i / j = "+ (i/j));
+        // Menggunakan System Modulus
+        System.out.println (" Modulus : ");
+        System.out.println (" i % j = " + (i%j));
+        
+        
+    }
+}
---
+++ Latihan-Java/src/main/java/pewarisan/sepeda.java
@@ -0,0 +1,20 @@
+package pewarisan;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+class sepeda{
+    int kecepatan = 0;
+    int gir = 0;
+//method
+    void ubahGir(int pertambahanGir){
+    gir= gir+ pertambahanGir;
+    System.out.println("Gir" + gir);
+}
+void tambahKecepatan (int pertambahanKecepatan) {
+kecepatan = kecepatan+ pertambahanKecepatan;
+System.out.println("Kecepatan:" + kecepatan);
+}
+}
