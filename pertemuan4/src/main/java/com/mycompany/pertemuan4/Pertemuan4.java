9bace92fca87a035ba03b62ae7f8cf51467e0d0c
Yuqimuna <felixstarfix3@gmail.com>
2024-04-24 22:50:17 +0700

    praktikum

---
+++ pertemuan4/src/main/java/com/mycompany/pertemuan4/Pertemuan4.java
@@ -0,0 +1,15 @@
+package com.mycompany.pertemuan4;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.io.*;
+import java.util.Scanner;
+public class Pertemuan4 {
+    public static void main (String[]args)throws IOException
+    {
+        Scanner input = new Scanner (System.in);
+        int i,j;
+    }
+}
---
+++ pertemuan5/src/main/java/com/mycompany/pertemuan5/Pertemuan5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan5;
+
+/**
+ *
+ * @author Lenovo
+ */
+import javax.swing.*;
+
+class Pertemuan5 {
+
+    public static void main(String[] args) {//agar dapat di run sebagai class utama
+        JFrame myWindow;
+        
+        myWindow = new JFrame();//pembuatan object
+        myWindow.setSize(300,200);//setting ukuran form
+        myWindow.setTitle("My First Java Program");//judul untuk form
+        myWindow.setVisible(true);//untuk menampilkan form
+    }
+}
